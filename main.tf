68820d4c2c1141c602bfe2101e4166ef69fff728
pwansi <pmongoue@yahoo.com>
2025-08-17 11:26:54 -0400

    add new feature branch

--- main.tf
+++ main.tf
@@ -54,3 +54,19 @@ resource "azurerm_linux_virtual_machine" "example" {
     version   = "latest"
   }
 }
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "storageaccountfortoday"
+  resource_group_name      = data.azurerm_resource_group.rg.name
+  location                 = data.azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
